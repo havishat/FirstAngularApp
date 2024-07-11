d2c6db2e36dbcf0fd597ec15d5d193718b91c53a
laxmi Tiruvuri <latiruvu@microsoft.com>
2024-07-11 14:47:44 -0700

    TodoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'FirstAngularApp';
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+  
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  deleteItem(description: string){
+    if (!description) return;
+    this.allItems.splice(this.allItems.findIndex(item => item.description === description), 1)
+  }
+
+  // editItem(description: string){
+  //   if (!description) return;
+  // }
 }
